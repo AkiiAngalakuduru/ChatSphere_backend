3e762dbd91aee6d6c1ffeaca2430b8115681f2d3
AkiiAngalakuduru <akhilangalakuduru005@gmail.com>
2026-08-11 17:15:14 +0530

    Implement the Auth system

--- server/src/config/redis.js
+++
@@ -1,8 +0,0 @@
-const { Redis } = require('@upstash/redis');
-
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_URL,
-  token: process.env.UPSTASH_REDIS_TOKEN
-});
-
-module.exports = redis;
